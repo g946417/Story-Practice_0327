dd03745cf047176fd1a0eb8f7806f83e3d237a15
Roy <s9212003@gmail.com>
2022-03-25 10:43:50 +0800

    Initial Commit

---
+++ Story Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Story Practice
+//
+//  Created by 陳俊維 on 2022/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
